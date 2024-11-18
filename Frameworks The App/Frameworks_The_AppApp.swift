76c7e0e7614caa24a923c98a8825f9e89093e4d2
Sbulelo Langa <tsakaneslanga@gmail.com>
2024-11-18 21:34:42 +0200

    Initial Commit

---
+++ Frameworks The App/Frameworks_The_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Frameworks_The_AppApp.swift
+//  Frameworks The App
+//
+//  Created by Sbulelo Langa on 2024/11/18.
+//
+
+import SwiftUI
+
+@main
+struct Frameworks_The_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
